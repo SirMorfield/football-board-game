33abe85e4b7d6aa931d8d41df7d691aa0937bed5
SirMorfield <joppe.koers@gmail.com>
2021-09-05 18:21:22 +0200

    revert renaming

--- versie1/versie1.ino
+++ versie1/versie1.ino
@@ -1,33 +1,35 @@
 #define NKOL 1
-#define ON 1
-#define OFF 0
+#define SLEEP_DELAY 60000
 
-const int SHIFT_REGISTER_CLOCK_INPUT = 12;	// HC595 LED shift register serial clock (high is shift), pin 11 van HC595 SWItCh
-const int SERIAL_REGISTER_CLOCK_INPUT = 13; // HC595 LED storage register clock (high is shift), pin 12 van HC595 SWITCH
-const int SERIAL_DATA_INPUT = 14;			// HC595 LED serial data, pin 14 on HC595
-const int PARALLEL_LOAD_INPUT = 15;			// HC597 SWITCH parallel load (low) of seriële shift, pin 13 van HC597
-const int SERIAL_DATA_INPUT = 16;			// HC587 serial data out, pin 9 van HC597 SWITCH
+// io pins
+#define SROUT_SHCP 12
+#define SROUT_STCP 13
+#define SROUT_DS 14
+#define SRIN_PL 15
+#define SRIN_DS 16
 
 uint8_t g_panLED[NKOL];
 uint8_t g_panINP[NKOL];
-uint8_t g_gamestatus;
-uint8_t g_activity;
+uint8_t g_game_is_running;
+uint8_t g_last_input_time;
 
 void setup()
 {
+	Serial.begin(115200);
+	Serial.println("\nGestart....");
 	memset(g_panINP, 0x00, sizeof(g_panINP));
-	pinMode(SHIFT_REGISTER_CLOCK_INPUT, OUTPUT);
-	digitalWrite(SHIFT_REGISTER_CLOCK_INPUT, LOW);
-	pinMode(SERIAL_REGISTER_CLOCK_INPUT, OUTPUT);
-	digitalWrite(SERIAL_REGISTER_CLOCK_INPUT, LOW);
-	pinMode(SERIAL_DATA_INPUT, OUTPUT);
-	digitalWrite(SERIAL_DATA_INPUT, LOW);
-	pinMode(PARALLEL_LOAD_INPUT, OUTPUT);
-	digitalWrite(PARALLEL_LOAD_INPUT, LOW);
-	pinMode(SERIAL_DATA_INPUT, INPUT);
+	pinMode(SROUT_SHCP, OUTPUT);
+	digitalWrite(SROUT_SHCP, LOW);
+	pinMode(SROUT_STCP, OUTPUT);
+	digitalWrite(SROUT_STCP, LOW);
+	pinMode(SROUT_DS, OUTPUT);
+	digitalWrite(SROUT_DS, LOW);
+	pinMode(SRIN_PL, OUTPUT);
+	digitalWrite(SRIN_PL, LOW);
+	pinMode(SRIN_DS, INPUT);
 	allLEDsOff();
-	g_gamestatus = OFF;
-	g_activity = 0;
+	g_game_is_running = false;
+	g_last_input_time = 0;
 }
 
 // set a single panle light either on or off
@@ -53,41 +55,38 @@ void set_panel_light(uint8_t kolom, uint8_t rij, uint8_t func)
 		g_panLED[kolom] ^= pat;
 }
 
-// sync shift registers with local buffers
+// read from g_panLED and write to the panel
 void sync_board()
 {
+	int column;
+	int row;
 	uint8_t sd_out;
 	uint8_t sd_inp;
 
-	for (uint32_t column = 0; column < NKOL; column++)
+	for (column = 0; column < NKOL; column++)
 	{
 		sd_out = g_panLED[column];
 		sd_inp = 0;
-		for (uint32_t row = 0; row < 8; row++)
+		for (row = 0; row < 8; row++)
 		{
 			sd_inp = sd_inp << 1;
-			if (digitalRead(SERIAL_DATA_INPUT) == LOW)
+			if (digitalRead(SRIN_DS) == LOW)
 			{
-				g_activity = millis();
+				Serial.print(row);
+				g_last_input_time = millis();
 				sd_inp |= 0x01;
 			}
-			digitalWrite(SERIAL_DATA_INPUT, sd_out & 1);
-			digitalWrite(SHIFT_REGISTER_CLOCK_INPUT, HIGH);
-			digitalWrite(SHIFT_REGISTER_CLOCK_INPUT, LOW);
+			digitalWrite(SROUT_DS, sd_out & 1);
+			digitalWrite(SROUT_SHCP, HIGH);
+			digitalWrite(SROUT_SHCP, LOW);
 			sd_out = sd_out >> 1;
 		}
 		g_panINP[column] = sd_inp;
 	}
-	digitalWrite(PARALLEL_LOAD_INPUT, LOW);
-	digitalWrite(SERIAL_REGISTER_CLOCK_INPUT, HIGH);
-	digitalWrite(SERIAL_REGISTER_CLOCK_INPUT, LOW);
-	digitalWrite(PARALLEL_LOAD_INPUT, HIGH);
-	if (g_gamestatus == OFF && (g_panINP[0] & (1 << 0)))
-	{
-		g_gamestatus = ON;
-		for (uint32_t i = 0; i < 8; i++)
-			set_panel_light(0, i, 0);
-	}
+	digitalWrite(SRIN_PL, LOW);
+	digitalWrite(SROUT_STCP, HIGH);
+	digitalWrite(SROUT_STCP, LOW);
+	digitalWrite(SRIN_PL, HIGH);
 }
 
 void allLEDsOff()
@@ -105,42 +104,46 @@ bool snooze_patern()
 	static uint32_t t0 = 0;
 	static uint16_t head = -1;
 	uint32_t t1;
-	uint16_t rij, kol;
+	uint16_t row, column;
 
-	rij = random(8);		// Kies random positie
 	t1 = millis();			// Haal huidige tijd
 	if (abs(t1 - t0) < 200) // In wachttijd?
 		return 1;
+	// Serial.print(t0);
 	head++;
 	if (head > 7)
 		head = 0;
-	rij = head;
-	for (kol = 0; kol < NKOL; kol++)
+	row = head;
+	for (column = 0; column < NKOL; column++)
 	{
-		set_panel_light(kol, rij, 1); // Set set_panel_lightl --> fills data inside shifters
+		set_panel_light(column, row, 1); // Set set_panel_lightl
 		if (head > 3)
-			set_panel_light(kol, rij - 4, 0);
+			set_panel_light(column, row - 4, 0);
 		if (head < 4)
-			set_panel_light(kol, 7 - 3 + rij, 0);
-		rij++;
+			set_panel_light(column, 7 - 3 + row, 0);
+		row++;
 	}
-	sync_board(); // Toon patroon --> writes to the leds
 	t0 = t1;
 }
 
 void check_activity()
 {
-	uint32_t t = millis();
-	if (abs(t - g_activity) > 60000)
+	uint32_t now = millis();
+
+	if (now - g_last_input_time > SLEEP_DELAY)
 	{
-		g_gamestatus = OFF;
-		g_activity = t;
+		g_game_is_running = true;
+		for (int i = 0; i < 8; i++)
+			set_panel_light(0, i, 0);
+		Serial.print("turn game on");
 	}
+	else
+		g_game_is_running = false;
 }
 
 void loop()
 {
-	if (g_gamestatus == OFF)
+	if (!g_game_is_running)
 		snooze_patern();
 	sync_board();
 	check_activity();
